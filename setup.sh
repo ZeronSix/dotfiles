959283fa5ad58d1616e5a36163fab7a73e236564
Vyacheslav Zhdanovskiy <zeronsix@gmail.com>
2018-08-09 11:07:29 +0300

    Add i3 config

--- setup.sh
+++ setup.sh
@@ -3,8 +3,9 @@
 # Make symbol links
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
+ln -sf ~/dotfiles/.i3status.conf ~/.i3status.conf
+ln -sf ~/dotfiles/.i3 ~/.i3
 
 # Install Vim plugins
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
-
